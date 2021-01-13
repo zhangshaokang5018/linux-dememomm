42d9e61602e884657e9de9d86468ed9dffb4a200
zhangshaokang <zhangsk1111@thundersoft.com>
2021-01-13 09:57:50 +0800

    add demomem6

--- demomem6/demomem.c
+++ demomem6/demomem.c
@@ -5,6 +5,7 @@
 #include <linux/device.h>
 #include <asm/uaccess.h>
 #include <linux/slab.h>
+#include <linux/miscdevice.h>
 
 #include "demomem.h"
 
@@ -16,9 +17,7 @@ struct demo_device
 {
     char *buffer; //将其改为指针
     int value;
-    struct cdev cdev;  
-    struct class *cls;
-    struct device *device;  
+    struct miscdevice *mdev;
 };
 static struct demo_device *demo_dev;
 
@@ -114,10 +113,16 @@ static struct file_operations demo_operation = {
     .unlocked_ioctl = demo_ioctl,
 };
 
+static struct miscdevice misc_struct = 
+{
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = "demomem",
+    .fops = &demo_operation,
+};
+
 static int __init demo_init(void)
 {
     int ret = -1;
-    dev_t dev_no;
 
     demo_dev = (struct demo_device *)kmalloc(sizeof(struct demo_device),GFP_KERNEL);
     if(!demo_dev)
@@ -136,54 +141,19 @@ static int __init demo_init(void)
     memset(demo_dev->buffer,0x00,BUF_SIZE);
     demo_dev->value = 1;
 
-    dev_no = MKDEV(MAJOR_NUM,MINOR_NUM);
-    printk(KERN_INFO "Enter: %s\n",__func__);
-    //init for demo char
-    cdev_init(&demo_dev->cdev, &demo_operation);
-    //register device number
-    ret = register_chrdev_region(dev_no,1,"demomem");//静态注册注册设备号时名字为demomem
+    demo_dev->mdev = &misc_struct;
+    ret = misc_register(demo_dev->mdev);
     if(ret < 0)
     {
-        ret = alloc_chrdev_region(&dev_no,0,1,"demomem");//在静态注册失败时，动态注册。0代表次设备号，1代表数量
-        if(ret < 0)
-        {
-            printk(KERN_ERR "failed to register device number\n");
-            goto ERROR_CHRDEV_REGION;
-        }
-       
+        printk(KERN_ERR "failed to register misc\n");
+        goto ERROR_MISC;
     }
 
-    ret = cdev_add(&demo_dev->cdev,dev_no,1);//注册一个主设备，默认有一个次设备，所以次设为1。
-    if(ret < 0)
-    {
-        printk(KERN_ERR "cdev add failed\n");
-        
-        goto ERROR_CDEV_ADD;
-    }
-    //create a demomem class in /sys/class/demo
-    demo_dev->cls = class_create(THIS_MODULE,"demo");
-    if(IS_ERR(demo_dev->cls))
-    {
-        ret = PTR_ERR(demo_dev->cls);
-        goto ERROR_CLASS_CREATE;
-    }
-    //create ademomem device in /sys/class/demo/demomem
-    demo_dev->device = device_create(demo_dev->cls,NULL,dev_no,NULL,"demomem");
-    if(IS_ERR(demo_dev->device))
-    {
-        ret = PTR_ERR(demo_dev->device);
-        goto ERROR_DEVICE_CREATE;
-    }
     printk(KERN_INFO "dome init done\n");
 
     return 0;
-ERROR_DEVICE_CREATE:
-    class_destroy(demo_dev->cls);
-ERROR_CLASS_CREATE:
-    cdev_del(&demo_dev->cdev);
-ERROR_CDEV_ADD:
-    unregister_chrdev_region(dev_no,1);
-ERROR_CHRDEV_REGION:
+
+ERROR_MISC:
     kfree(demo_dev->buffer);//从这开始后面无论在那步骤失败都要释放demo_dev.buffer
     demo_dev->buffer = NULL;
 ERROR_MALLOC_BUFFER:
@@ -198,8 +168,7 @@ static void __exit demo_exit(void)
 {
     
     printk(KERN_INFO "Enter: %s\n",__func__);
-    cdev_del(&demo_dev->cdev);
-    unregister_chrdev_region(MKDEV(MAJOR_NUM,MINOR_NUM),1);
+    misc_deregister(demo_dev->mdev);
 
     kfree(demo_dev);
     demo_dev = NULL;
